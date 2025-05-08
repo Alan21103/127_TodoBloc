00f87f419d54b338ec07465e010368ae114a10dd
Alan21103 <syahlansaputra03@gmail.com>
2025-05-08 22:01:46 +0700

    resolve ignore must be imutable

--- lib/bloc/todo_state.dart
+++ lib/bloc/todo_state.dart
@@ -7,6 +7,7 @@ final class TodoInitial extends TodoState {}
 
 final class TodoLoading extends TodoState {}
 
+// ignore: must_be_immutable
 final class TodoLoaded extends TodoState {
   final List<Todo> todos;
   DateTime? selectedDate;
