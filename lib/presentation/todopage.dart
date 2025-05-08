3e9a5a8449c185a73d1794c24b69544d0afd3019
Alan21103 <syahlansaputra03@gmail.com>
2025-05-08 21:23:07 +0700

    implementasi tampilan daftar todo dengan pengecekan loading dan kosong

--- lib/presentation/todopage.dart
+++ lib/presentation/todopage.dart
@@ -97,6 +97,29 @@ class TodoPage extends StatelessWidget {
                   ],
                 ),
               ),
+              SizedBox(height: 16.0),
+              Expanded(
+                child: BlocBuilder<TodoBloc, TodoState>(
+                  builder: (context, state) {
+                    if (state is TodoLoading) {
+                      return Center(child: CircularProgressIndicator());
+                    }else if (state is TodoLoaded) {
+                      if(state.todos.isEmpty){
+                        return Center(child: Text('Todo list is empty'));
+                      }
+                      return ListView.builder(
+                        itemCount: state.todos.length,
+                        itemBuilder: (context, index){
+                          final todo = state.todos[index];
+                          return Container(
+                            margin: EdgeInsets.only(bottom: 8.0),
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
+              ),
             ],
           ),
         ),
