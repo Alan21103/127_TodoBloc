60856e0081afb7c12a89388a4598a540c402d2bb
Alan21103 <syahlansaputra03@gmail.com>
2025-05-08 21:34:06 +0700

    Mengimport Todo page ke main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:counter_convert_todo/presentation/todopage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TodoPage(),
     );
   }
 }
